34200364b7d8288236bfde65afb7d584ae1f1023
Nikoly <turnik3580@gmail.com>
2020-10-28 16:49:50 +0300

    edit service & test to 100%

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,6 +21,15 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCashBackNoMore() {
         assertEquals(service.remain(1000), 0);
+    }
+
+   @Test
+    public void shouldCashBackBoundaryLessThan1000() {
+        Assertions.assertEquals(service.remain(999), 1);
+    }
+    @Test
+    public void shouldCashBackBoundary1() {
+        Assertions.assertEquals(service.remain(1), 999);
     }
 }
 
